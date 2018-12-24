abff0767a706d3f63aa7b01e1c5fc548784af7cb
zdq0394 <zdq123.hn@163.com>
2018-12-24 14:48:23 +0800

    add const in v1alpha1/register.go

--- pkg/apis/redis/v1alpha1/register.go
+++ pkg/apis/redis/v1alpha1/register.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -24,8 +25,20 @@ import (
 	redis "github.com/zdq0394/redis-cluster-operator/pkg/apis/redis"
 )
 
+const (
+	version      = "v1alpha1"
+	RCKind       = "RedisCluster"
+	RCNamePlural = "rediscluster"
+	RCScope      = apiextensionsv1beta1.NamespaceScoped
+)
+
 // SchemeGroupVersion is group version used to register these objects
-var SchemeGroupVersion = schema.GroupVersion{Group: redis.GroupName, Version: "v1alpha1"}
+var SchemeGroupVersion = schema.GroupVersion{Group: redis.GroupName, Version: version}
+
+// VersionKind takes an unqualified kind and returns back a Group qualified GroupVersionKind
+func VersionKind(kind string) schema.GroupVersionKind {
+	return SchemeGroupVersion.WithKind(kind)
+}
 
 // Kind takes an unqualified kind and returns back a Group qualified GroupKind
 func Kind(kind string) schema.GroupKind {
