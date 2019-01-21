7e2b8c4f8c9c39d919fff19dae716f37618b8088
zdq0394 <zdq123.hn@163.com>
2019-01-21 10:06:59 +0800

    refactor

--- operator/rediscluster/handler/generator.go
+++ operator/rediscluster/handler/generator.go
@@ -4,6 +4,31 @@ import (
 	"fmt"
 )
 
+const (
+	redisHeadlessPort     = 6379
+	redisHeadlessPortName = "redis-port"
+
+	redisAccessPort     = 6379
+	redisAccessPortName = "redis-port"
+)
+
+const (
+	svcHeadlessNamePrefix = "rch"
+	svcAccessNamePrefix   = "rca"
+	statefulsetNamePrefix = "rcs"
+	configMapNamePrefix   = "rcfg"
+	bootPodNamePrefix     = "rcboot"
+)
+
+var (
+	terminationGracePeriodSeconds int64 = 20
+)
+
+const (
+	redisClusterBootImage         = "zdq0394/redis-cluster-boot:1.2"
+	redisClusterBootClusterDomain = "cluster.local"
+)
+
 func generateName(prefix, name string) string {
 	return fmt.Sprintf("%s-%s", prefix, name)
 }
